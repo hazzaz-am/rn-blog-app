4c33ffb23b6fe0c7a07349ebf20b8d656708e136
hazzaz-am <hazzazabdulmannan@gmail.com>
2026-03-30 10:57:00 +0600

    feat: add nativewind for tailwind support and redesign feed layout and screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,28 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import "./global.css";
 
 export default function RootLayout() {
 	return (
-		<SafeAreaView style={{ flex: 1 }}>
-			<Stack
-				screenOptions={{
-					headerShown: false,
-				}}
-			/>
-			<StatusBar style="dark" />
-		</SafeAreaView>
+		<GestureHandlerRootView style={{ flex: 1 }}>
+			<SafeAreaView style={{ flex: 1 }}>
+				<Stack
+					screenOptions={{
+						headerShown: false,
+					}}
+				>
+					<Stack.Screen
+						name="(feed)/create"
+						options={{
+							presentation: "modal",
+							animation: "slide_from_bottom",
+						}}
+					/>
+				</Stack>
+				<StatusBar style="dark" />
+			</SafeAreaView>
+		</GestureHandlerRootView>
 	);
 }
